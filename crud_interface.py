43b963812b420c0bcde34a54d043c7635726007d
BiaHawk <48568610+BiaHawk@users.noreply.github.com>
2021-11-29 16:30:50 -0300

    botões e conexão com DB

--- crud_interface.py
+++ crud_interface.py
@@ -1,20 +1,16 @@
 from tkinter import *
 from tkinter import ttk
-
-from data_helper import DBConnection
-
+import tkinter.messagebox
+import pymysql
 
 class CRUDInterface:
     def __init__(self, root):
-         # Instanciando a conexão com o banco.
-        self.banco = DBConnection()
-
         self.root = root
         titlespace = " "
         self.root.title(102 * titlespace + "Conection")
         self.root.geometry("776x700+300+0")
         self.root.resizable(width = True, height= True)
-
+        #Interface Inicial
         MainFrame = Frame(self.root, bd=10, width= 770, height= 700, relief = RIDGE, bg= 'light salmon')
         MainFrame.grid()
 
@@ -35,45 +31,70 @@ class CRUDInterface:
 
         self.title= Label(TitleFrame, font=('Cambria', 30, 'bold'), text= 'Implementação CRUD IAAD', bd=7)
         self.title.grid(row=0, column=0, padx=132)
-
-        self.NomeEscola= Label(LeftFrameY, font=('Cambria', 12, 'bold'), text= 'Nome da Escola', bd=7)
-        self.NomeEscola.grid(row=1, column=0, sticky=W, padx =5)
-        self.CaixaNomeEscola = Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left')
-        self.CaixaNomeEscola.grid(row=1, column=1, sticky=W, padx=5)
+        #Variaveis
+        Escola = StringVar()
+        EscolaID = StringVar()
+        NomeEntidade = StringVar()
+        ValorPrevisto = StringVar()
         
-        self.CNPJEscola= Label(LeftFrameY, font=('Cambria', 12, 'bold'), text= 'CNPJ da Escola', bd=7)
-        self.CNPJEscola.grid(row=2, column=0, sticky=W, padx =5)
-        self.CaixaCNPJEscola = Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left')
-        self.CaixaCNPJEscola.grid(row=2, column=1, sticky=W, padx=5)
-
+        #funções dos botões e conexão com o BD
+        def ADD():
+            if Escola.get() == "" or EscolaID.get() == "" or NomeEntidade.get() == "" or ValorPrevisto.get() == "":
+                tkinter.messagebox.showerror("Connection", "Valores não existentes no DB")
+            else:
+                sqlconnection = pymysql.connect(host ="", user = "", password = "", database = 'mydb')
+                nav = sqlconnection.cursor()
+                nav.execute("insert into valor_investido (nome_escola, cod_escola, nome_eex, valor_previsto) values(%s, %s, %s, %s)",(Escola.get(), EscolaID.get(), NomeEntidade.get(), ValorPrevisto.get()))
+                sqlconnection.commit()
+                sqlconnection.close()
+                tkinter.messagebox.showinfo("Connection", "Sucesso")
+                
+        #Layout das entradas
+        self.NomeEscola= Label(LeftFrameY, font=('Cambria', 12, 'bold'), text= 'Nome da Escola', bd=7)
+        self.NomeEscola.grid(row=0, column=0, sticky=W, padx =5)
+        self.CaixaNomeEscola = Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left', textvariable= Escola)
+        self.CaixaNomeEscola.grid(row=0, column=1, sticky=W, padx=5)
+        
         self.EscolaID= Label(LeftFrameY, font=('Cambria', 12, 'bold'), text= 'ID da Escola', bd=7)
-        self.EscolaID.grid(row=3, column=0, sticky=W, padx =5)
-        self.EscolaID= Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left')
-        self.EscolaID.grid(row=3, column=1, sticky=W, padx=5)
+        self.EscolaID.grid(row=1, column=0, sticky=W, padx =5)
+        self.EscolaID= Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left', textvariable=EscolaID)
+        self.EscolaID.grid(row=1, column=1, sticky=W, padx=5)
 
+        
+        self.NomeEntidade= Label(LeftFrameY, font=('Cambria', 12, 'bold'), text= 'Nome Entidade Executora', bd=7)
+        self.NomeEntidade.grid(row=2, column=0, sticky=W, padx =5)
+        self.CaixaNomeEntidade = Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left', textvariable=NomeEntidade)
+        self.CaixaNomeEntidade.grid(row=2, column=1, sticky=W, padx=5)
+
+        self.ValorPrevisto= Label(LeftFrameY, font=('Cambria', 12, 'bold'), text= 'Valor Previsto', bd=7)
+        self.ValorPrevisto.grid(row=3, column=0, sticky=W, padx =5)
+        self.ValorPrevisto= Entry(LeftFrameY, font=('Cambria', 12, 'bold'), bd=5, width= 44, justify= 'left', textvariable= ValorPrevisto)
+        self.ValorPrevisto.grid(row=3, column=1, sticky=W, padx=5)
+        
+        
+        #Table View
         scroll_y = Scrollbar(LeftFrame, orient=VERTICAL)
-        self.TableEscola = ttk.Treeview(LeftFrame, height=14, columns=('nomescola', 'cnpj', 'id','value'), yscrollcommand=scroll_y.set)
+        self.TableEscola = ttk.Treeview(LeftFrame, height=14, columns=('escola','id', 'entidade','value'), yscrollcommand=scroll_y.set)
         scroll_y.pack(side = RIGHT, fill=Y)
 
-        self.TableEscola.heading('nomescola', text='Nome da Escola')
-        self.TableEscola.heading('cnpj', text= 'CNPJ da Escola')
+        self.TableEscola.heading('escola', text='Nome da Escola')
         self.TableEscola.heading('id', text= 'ID da Escola')
+        self.TableEscola.heading('entidade', text= 'Entidade Executora')
         self.TableEscola.heading('value', text= 'Valor Total Previsto')
         self.TableEscola['show']='headings'
 
-        self.TableEscola.column('nomescola', width = 100)
-        self.TableEscola.column('cnpj', width= 50)
-        self.TableEscola.column('id', width=80)
-        self.TableEscola.column('value', width=50)
+        self.TableEscola.column('escola', width = 100)
+        self.TableEscola.column('entidade', width= 60)
+        self.TableEscola.column('id', width=70)
+        self.TableEscola.column('value', width=70)
         self.TableEscola.pack(fill= BOTH, expand=1)
-
+        
+        #Botões
         self.Display = Button(RightFrameYt, font=('Cambria', 16, 'bold'), text= 'DISPLAY', bd=4, pady=1, padx=24, width = 8, height = 2,).grid(row=0,column =0, padx=1)
         self.Search = Button(RightFrameYt, font=('Cambria', 16, 'bold'), text= 'SEARCH', bd=4, pady=1, padx=24, width = 8, height = 2,).grid(row=1,column =0, padx=1)
-        self.Add = Button(RightFrameYt, font=('Cambria', 16, 'bold'), text= 'ADD', bd=4, pady=1, padx=24, width = 8, height = 2,).grid(row=2,column =0, padx=1)
+        self.Add = Button(RightFrameYt, font=('Cambria', 16, 'bold'), text= 'ADD', bd=4, pady=1, padx=24, width = 8, height = 2, command=ADD).grid(row=2,column =0, padx=1)
         self.Update = Button(RightFrameYt, font=('Cambria', 16, 'bold'), text= 'UPDATE', bd=4, pady=1, padx=24, width = 8, height = 2,).grid(row=3,column =0, padx=1)
         self.Delete = Button(RightFrameYt, font=('Cambria', 16, 'bold'), text= 'DELETE', bd=4, pady=1, padx=24, width = 8, height = 2,).grid(row=4,column =0, padx=1)
-        
-        
 
 
 if __name__=='__main__':
